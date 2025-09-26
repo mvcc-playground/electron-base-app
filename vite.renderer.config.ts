cb27df488a54934ac5bcc52d856343c5ff67ec27
matheusvcouto <140331653+matheusvcouto@users.noreply.github.com>
2025-09-26 09:57:18 -0300

    update eslint and vite render

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,10 @@
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    entries: ["index.html"], // Ou 'src/index.html' se estiver em subdir
+  },
+});
