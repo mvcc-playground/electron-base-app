1d67e0cb52a79742c3b67602bb1a8418e5b1e75c
matheusvcouto <140331653+matheusvcouto@users.noreply.github.com>
2025-09-27 22:14:26 -0300

    fix render vite config

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,10 +1,4 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    entries: ["index.html"], // Ou 'src/index.html' se estiver em subdir
-  },
-});
+export default defineConfig({});
